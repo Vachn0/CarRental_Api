452be98639dbd04c33592fce0bf4ef59c3971256
Vachno <vachnadzesaba@gmail.com>
2024-06-06 21:13:03 +0400

    Add project files.

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCar.models;
+
+namespace RentCar.DTO;
+
+public class UserDTO
+{
+    // This Class Is Used For Inputting Data To The Database.
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+}
